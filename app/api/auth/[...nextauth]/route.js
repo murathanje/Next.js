b26365aeeedb717ea4b6d0fa1549bc5606f8e72e
Murathan Kağan BAYRAM <murathankagan13@gmail.com>
2023-10-14 16:09:29 +0300

    SignIn and SignOut was done

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,52 +1,47 @@
-import mongoose from 'mongoose';
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import { connectToDB } from '@utils/database';
+
 import User from '@models/user';
-
-
-
+import { connectToDB } from '@utils/database';
+const password = encodeURIComponent("<password>");
 const handler = NextAuth({
-    providers: [    
+    providers: [
         GoogleProvider({
-            clientID: process.env.GOOGLE_ID,
+            clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
- 
-    async session({ session }){
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        })
+    callbacks: {
+        async session({ session }) {
+            // store the user id from MongoDB to session
+            const sessionUser = await User.findOne({ email: session.user.email });
+            session.user.id = sessionUser._id.toString();
 
-        session.user.id = sessionUser._id.toString();
-        return session;
-    }, 
+            return session;
+        },
+        async signIn({ account, profile, user, credentials }) {
+            try {
+                await connectToDB();
 
-    async signIn({ profile }){
-        try {
-            await connectToDB();
+                // check if user already exists
+                const userExists = await User.findOne({ email: profile.email });
 
-            // Check if a user already exists
-            const userExists = await User.findOne({
-                email: profile.email
-            })
+                // if not, create a new document and save user in MongoDB
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture,
+                    });
+                }
 
-            //if not, create a new user
-            if(!userExists){
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace("","".toLowerCase()),
-                    image: profile.picture
-                })
+                return true
+            } catch (error) {
+                console.log("Error checking if user exists: ", error.message);
+                return false
             }
-
-            return true;
-        } catch (error) {
-            console.log(error);
-            return false;
-        }
+        },
     }
 })
 
-export { handler as GET, handler as POST };  
+export { handler as GET, handler as POST }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,18 +5,18 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const { data: session} = useSession();
+  const { data: session } = useSession();
   const [providers, setProviders] = useState(null);
   const [toggleDropDown, settoggleDropDown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     }
 
-    setProviders();
+    setUpProviders();
   }, [])
 
   return (
@@ -32,23 +32,21 @@ const Nav = () => {
         <p className="logo_text">Promtopia</p>
       </Link>
 
-      
-
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
         {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
-            <Link href="/create-promt" className='black_btn' >Create Post</Link>
-            <button className="outline_btn" type='button'>Sign Out</button>
+            <Link href="/create-prompt" className='black_btn' >Create Post</Link>
+            <button className="outline_btn" type='button' onClick={signOut}>Sign Out</button>
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className='rounded-full'
                 alt='profile'
               />
-            </Link>
+            </Link> 
           </div>
         ) : (
           <div>
@@ -62,6 +60,7 @@ const Nav = () => {
                 >
                   Sign In
                 </button>
+
               ))}
           </div>
         )}
@@ -73,7 +72,7 @@ const Nav = () => {
         {session?.user ? (
           <div className='flex'>
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className='rounded-full'
@@ -122,6 +121,7 @@ const Nav = () => {
                 >
                   Sign In
                 </button>
+              
               ))}
           </div>
         )}
