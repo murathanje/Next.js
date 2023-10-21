e6f75c13aa437d289787191eb66c0e80e3ec3ec8
Murathan Kağan BAYRAM <murathankagan13@gmail.com>
2023-10-21 17:56:15 +0300

    Last commit before the tasks

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import PromtCard from "./PromtCard"
 
-const Profile = () => {
+const Profile = ({name, desc, data, handleEdit, handleDelete}) => {
   return (
-    <div>Profile</div>
+     <section className="w-full">
+          <h1 className="head_text text_left">
+            <span className="blue_gradient">{name} Profile</span>  
+          </h1>
+          <p className="desc text-left">{desc}</p>
+
+          <div className="mt-10 prompt_layout">
+              {data.map((post) => (
+                <PromtCard 
+                  key={post.id}
+                  post={post}
+                  handleEdit={() => handleEdit && handleEdit(post)}
+                  handleDelete={() => handleDelete(post)}
+                />
+              ))}
+          </div>
+     </section>
   )
 }
 
